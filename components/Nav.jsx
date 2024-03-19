f37a216117875bc01989486374ac34a494064c49
pengkiang <pengkiangtan@gmail.com>
2024-03-19 15:00:47 +0800

    no more changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -52,7 +52,7 @@ const Nav = () => {
                     <p className='text-[75px] font-grunge'><span className='text-transparent md:text-black'>rto</span></p>
                 </Link>
             </div>
-            <div className='w-[40%] min-w-[285px] ml-[55px] md:ml-[70px]'>
+            <div className='w-[40%] min-w-[285px] ml-[33px] md:ml-[70px]'>
         
                 <div className="mt-3 flex rounded- shadow- w-full h-[70px] relative">
                     <div className="relative flex flex-grow-[5] items-stretch focus-within:z-10">
