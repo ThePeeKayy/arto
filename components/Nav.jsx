7b80d30548d113440e018cf38602f5d0e9b81dc7
pengkiang <pengkiangtan@gmail.com>
2024-03-22 01:09:32 +0800

    go sleep

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -55,7 +55,7 @@ const Nav = () => {
     return (
         <>
         {showCart && <Cart />}
-        <nav className='flex sm:justify-between justify-evenly h-auto pb-2 sm:w-full w-[90%] relative' >
+        <nav className='flex sm:justify-between justify-evenly h-auto pb-2 pr-4 w-full relative' >
             <div className='flex gap-2 ml-6 flex-center flex-row items-center'>
                 <Link href='/' className='flex flex-row absolute top-[-10px] left-4'>
                     <Image src={logo} width={35} height='auto' alt='logo' style={{objectFit: "contain"}}/>
