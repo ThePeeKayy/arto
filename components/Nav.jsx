1c0c3617967a38f996dc85feb202a8fb36d4d5fa
pengkiang <pengkiangtan@gmail.com>
2024-03-23 23:22:40 +0800

    googoo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -56,7 +56,7 @@ const Nav = () => {
         <>
         {showCart && <Cart />}
         <nav className='flex sm:justify-between justify-around h-auto pb-2 sm:mr-0 mr-[50px] sm:w-full w-[85%] relative' >
-            <div className='flex gap-2 ml-6 mr-[10px] flex-center flex-row items-center'>
+            <div className='flex gap-2 ml-6 flex-center flex-row items-center'>
                 <Link href='/' className='flex flex-row absolute top-[-10px] left-4'>
                     <Image src={logo} width={35} height='auto' alt='logo' style={{objectFit: "contain"}}/>
                     <p className='text-[75px] font-grunge'><span className='text-transparent md:text-black'>rto</span></p>
